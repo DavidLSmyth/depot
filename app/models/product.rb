19a968211128a904ce324ffad5778ff701c3421f
DavidLSmyth <d.smyth10@nuigalway.ie>
2016-10-05 13:07:55 +0100

    Tried to add the time to the side

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+	@@time =Time.new
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -6,4 +7,7 @@ class Product < ActiveRecord::Base
   		with: %r{\.(gif|jpg|png|jpeg)\Z}i,
   		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+	def gettime()
+		return "The time is: "+to_s(@@time.now())
+	end
 end
