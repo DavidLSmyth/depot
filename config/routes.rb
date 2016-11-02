889e4d9626ed217eeb5e630a11d50f43686329cb
DavidLSmyth <d.smyth10@nuigalway.ie>
2016-11-02 20:54:20 +0000

    tried to add an atom feed

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,14 @@ Rails.application.routes.draw do
   resources :carts
 
   get 'store/index'
-
+	
 	get 'carts/remove' => 'settings#remove', :as => :remove
 
 
 	#delete "remove", path: "destroy/:id", on: :product
-
-  resources :products
+	resources :products do
+		get :who_bought, on: :member
+	end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
